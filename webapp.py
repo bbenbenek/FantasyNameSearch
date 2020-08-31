bcfb1dedf6e03683cdeaace66031d8b376caa4b6
Brian Benbenek <brian.benbenek@gmail.com>
2020-08-30 21:11:51 -0400

    update

--- webapp.py
+++ webapp.py
@@ -59,10 +59,12 @@ def index():
     #return render_template('index.html', form=search_form, message=message)
     return Response(stream_with_context(stream_template('index.html', form=search_form, messages=messages))) # Stream results instead of pre-loading
 
+"""
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+"""
 
 @app.route('/league_search')
 def leaguesearchPage():
